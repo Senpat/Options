71f36a2b19da97cf81f105943fe441ebd543170b
Sara <sarayaoliu@gmail.com>
2021-03-28 03:28:29 -0400

    results formatting and button hover

--- app/src/components/ResultRow.js
+++ app/src/components/ResultRow.js
@@ -1,11 +1,28 @@
-
+import '../style/ResultRow.css'
 
 const ResultRow = ({rank,option,estimatedreturn}) => {
 
-
+	var strArray = option.description.split(" ")
+	if(!strArray[4].includes(".")) {
+		strArray[4] += ".00";
+	}
+	var subStrIdx = strArray[4].indexOf(".")
+	if(strArray[4].substring(subStrIdx).length < 3) {
+		strArray[4] += "0"
+	}
+	//console.log(strArray[6])
+	// if(strArray[6].substring(0, 1) == "(") {
+	// 	strArray[5] += strArray[6];
+	// 	strArray[6] = strArray[7];
+	// }
     return (
         <div>
-            <p>{rank}: {option.description}, Estimated return: {estimatedreturn}%</p>
+            <p class='row'>{strArray[0]}&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;{strArray[1]} {strArray[2]}, {strArray[3]}
+            &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;{strArray[4]}
+            &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;{estimatedreturn}%
+            &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;{strArray[5]}
+            &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;{strArray[6]}
+            </p>
         </div>
     )
 
