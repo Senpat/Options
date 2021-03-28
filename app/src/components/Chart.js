52e1fa57503adc5f9edc1710d8b0d49b71c21fda
Sara <sarayaoliu@gmail.com>
2021-03-28 06:27:05 -0400

    updated Chart.js

--- app/src/components/Chart.js
+++ app/src/components/Chart.js
@@ -1,13 +1,10 @@
 import {useState,useEffect} from 'react'
-
+import {TradingViewEmbed, widgetType} from "react-tradingview-embed"
 import axios from 'axios'
-
 import {marketstackkey,tdkey} from "./../apikey.js"
-
 import Widget from "./Widget"
 import AreaChart from "./StockChart"
 import './../style/chartstyle.css'
-
 const Chart = ({ticker,stockprice,setStockprice}) => {
     const [stockdata,setStockdata] = useState({
         'name':'nostock'
@@ -28,6 +25,7 @@ const Chart = ({ticker,stockprice,setStockprice}) => {
     }, [ticker])
 
     useEffect(() => {
+        //console.log('effect ' + ticker)
         console.log('effect2 ' + ticker)
         axios
             .get('http://api.marketstack.com/v1/tickers/'+ticker.toUpperCase()+'/eod/latest?access_key=' + marketstackkey)
@@ -54,22 +52,27 @@ const Chart = ({ticker,stockprice,setStockprice}) => {
     }, [ticker])
     console.log(stockchartdata)*/
     if(stockdata.name === 'nostock') return (<div></div>)
-
     return (
         <div class='chart'>
             <h1>{stockdata.name} ({stockdata.stock_exchange.acronym}:{stockdata.symbol})</h1>
-            <h3>Current Price: ${stockprice}</h3>
-
-            <Widget symbol={stockdata.symbol} exchange={stockdata.stock_exchange.acronym}/>
-
+            {/*<h3>Current Price: ${stockprice}</h3>*/}
+            {/*<div class="widget">
+                <TradingViewEmbed 
+                    widgetType={widgetType.SYMBOL_OVERVIEW}
+                    widgetConfig={{
+                        "symbols": [[
+                            stockdata.symbol,
+                            stockdata.symbol
+                        ]]
+                        
+                    }}
+                />
+                </div>*/}
+            <Widget stockdata={stockdata}/>
             {/*<AreaChart type="svg" data={stockchartdata} />*/}
         </div>
         
     )
-
     
-
-
 }
-
 export default Chart
